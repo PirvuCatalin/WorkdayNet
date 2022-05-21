5a6de19634c4bc5e6ff090ab11ef73d21b87dadb
Catalin Pirvu <Catalin.Pirvu@viavisolutions.com>
2022-05-21 11:00:51 +0300

    added unit tests + basic working examples without going to next workday

--- WorkdayNet/WorkdayCalendar.cs
+++ WorkdayNet/WorkdayCalendar.cs
@@ -1,5 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace TestCalendar
+namespace WorkdayNet
 {
     class WorkDay
     {
@@ -36,25 +41,25 @@ namespace TestCalendar
 
         public DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays)
         {
-            return new DateTime();
-
-            // the actual increment is decimal workdays and the expected precision is minutes, so we can discard seconds -> miliseconds ...
-            // e.g. for 24-05-2004 19:03 with an addition of 44.723656 work days is 27-07-2004 13:47
-            // we can just focus on the hours + minutes part 
-            //      0.723656 * 8h = 5.789248 h => that means we're going to reach 13:00 ->
-            //      0.789248 * 60m = 47.35488 m => that means the final time is 13:47
-            //      we could go further and see that 0.35488 * 60s = 21.2928 s => the final time is actually 13:47:21
-            // but we won't
-            //
-            //  Important note: TimeSpan provides helpful methods, but in our case the workdays = 8 hours so we'll just implement new method
+            TimeSpan timeSpan = IncrementToTimeSpan(incrementInWorkdays);
+            return startDate.Add(timeSpan);
         }
 
-        public TimeSpan incrementToTimeSpan(decimal incrementInWorkdays)
+        // the actual increment is decimal workdays and the expected precision is minutes, so we can discard seconds -> miliseconds ...
+        // e.g. for 24-05-2004 19:03 with an addition of 44.723656 work days is 27-07-2004 13:47
+        // we can just focus on the hours + minutes part 
+        //      0.723656 * 8h = 5.789248 h => that means we're going to reach 13:00 ->
+        //      0.789248 * 60m = 47.35488 m => that means the final time is 13:47
+        //      we could go further and see that 0.35488 * 60s = 21.2928 s => the final time is actually 13:47:21
+        // but we won't
+        //
+        //  Important note: TimeSpan provides helpful methods (FromDays), but in our case the workdays = variable hours so we'll just use it for passing data
+        public TimeSpan IncrementToTimeSpan(decimal incrementInWorkdays)
         {
             // would be integer, but can overflow
             decimal days = Math.Truncate(incrementInWorkdays);
 
-            decimal hoursIncrement = (incrementInWorkdays - days) * 8;
+            decimal hoursIncrement = (incrementInWorkdays - days) * GetWorkdayInHours();
             decimal hours = Math.Truncate(hoursIncrement);
 
             decimal minutesIncrement = (hoursIncrement - hours) * 60;
@@ -62,7 +67,25 @@ namespace TestCalendar
 
             // just skip seconds i.e. 0
             return new TimeSpan((int)days, (int)hours, (int)minutes, 0);
-            //return (int)days;
+        }
+
+        public decimal GetWorkdayInHours()
+        {
+            // note that this may introduce rounding issues from TimeSpan's TotalHours, but it's a good start
+            // take into account minutes and also hour could be next day... so that:
+
+
+            DateTime start = new DateTime(1, 1, 1, workDay.StartHours, workDay.StartMinutes, 0);
+            DateTime end = new DateTime(1, 1, 1, workDay.StopHours, workDay.StopMinutes, 0);
+
+            if (workDay.StopHours < workDay.StartHours)
+            {
+                end = end.AddDays(1);
+            }
+
+            TimeSpan difference = end - start;
+
+            return (decimal)difference.TotalHours;
         }
     }
 }
